e6665126a3ece5b331939b48c90382ca87244cab
Akshat Garg <coolakshat24@gmail.com>
2017-06-11 17:18:03 +0530

    Add Routes

--- app/Routes.js
+++ app/Routes.js
@@ -3,16 +3,12 @@ import ReactRouter from 'react-router';
 import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router';
 import Main from './Main';
 import Home from './Home';
-// import Profile from './Profile';
-// import Article from './Article';
-// import Startup from './Startup';
-// import ForgotPassword from './ForgotPassword';
-// import WalkThrough from './WalkThrough';
 
 var routes = (
   <Router history={browserHistory}>
     <Route path='/' component={Main}>
-    	<Route path='home' component={Home}  />
+    	<IndexRoute component={Home} />
+    	<Route path='*' component={Home} />
     </Route>
   </Router>
 );
