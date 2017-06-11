f93bf97dc104a1a7a789e93cb4d5abf25adf4a34
Akshat Garg <coolakshat24@gmail.com>
2017-06-11 14:26:52 +0530

    Add Main.js

--- app/Main.js
+++ app/Main.js
@@ -4,6 +4,19 @@ require('./app.css');
 
 export default class Main extends React.Component {
 
+  componentWillMount = () => {
+
+    if(localStorage.getItem('items'))
+    {
+      //Do nothing
+    }
+    else
+    {
+      const temp=[];
+      localStorage.setItem('items', JSON.stringify(temp));
+    }    
+  }
+
   render() {
     return (
       <div className='main-container'>
